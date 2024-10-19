f9e9998522e4d4b286d9e4b88a1938ae66632df3
lufemoal19 <lufemoal19@gmail.com>
2024-10-18 23:07:03 -0600

    fix main.py

--- main/main.py
+++ main/main.py
@@ -7,8 +7,11 @@
 """
 
 def hello_user(user_name):
-    return f"HELLO {user_name.capitalize} HOW ARE YOU DOING?"
+    return f"HELLO {user_name} HOW ARE YOU DOING?"
 
 def main():
-    user_name = "Camila"
+    user_name = "CAMILA"
     print(hello_user(user_name))
+
+if __name__ == "__main__":
+    main()
