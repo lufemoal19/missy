de2af28a21ecdec8eb7ed6957db74c02cfff61d8
lufemoal19 <lufemoal19@gmail.com>
2024-10-18 22:57:56 -0600

    add some random code to main.py

--- main/main.py
+++ main/main.py
@@ -1,1 +1,14 @@
-print(':D')
+"""
+@author Camila Cabezas
+@author Felipe Morera
+@about main.py -> simple Python script
+@version 1.0
+@since 10/18/2024
+"""
+
+def hello_user(user_name):
+    return f"HELLO {user_name.capitalize} HOW ARE YOU DOING?"
+
+def main():
+    user_name = "Camila"
+    print(hello_user(user_name))
